8208dfa63c6ad80df125550b61e3dae90e147d90
Mario Garcia <iscmariog@gmail.com>
2016-03-19 15:32:33 -0600

    Update caesar.py

--- python/caesar.py
+++ python/caesar.py
@@ -16,7 +16,7 @@ def caesar(x, message2, abc):
       n = n + 1
 
 message = "hello world"
-message2 = list("hello world")
+message2 = list(message)
 print("Mensaje original: ", message)
 x = 3
 abc=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
